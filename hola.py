16d68397fedee56bb55a680737f7559de9cfa3a4
LizethGlez <61913321+LizethGlez@users.noreply.github.com>
2020-05-20 03:09:11 -0500

    libros

--- hola.py
+++ hola.py
@@ -11,21 +11,11 @@ def url_principal():
 def url_librosJ():
  	tipos = [
  		"El psicoanalísta",
- 		"Jaque al psicoanalísta"
- 		"Un final perfecto"
+ 		"Jaque al psicoanalísta",
+ 		"Un final perfecto",
  		"La historia del loco"
  	]
  	return render_template("librosJ.html", tipos=tipos, nombre="Titulos")
-
- @app.route("/librosH")
-def url_librosH():
- 	tipos = [
- 		"El extranjero",
- 		"El necronomicon"
- 		"Los gatos de ulthar"
- 		"El caso de Charles Dexter Ward"
- 	]
- 	return render_template("librosH.html", tipos=tipos, nombre="Titulos")
 
 if __name__=="__main__":
 	app.run(debug=True)
