ca128eda61897e900c3f04d3e749274e9e2dcc2b
LizethGlez <61913321+LizethGlez@users.noreply.github.com>
2020-05-20 02:31:46 -0500

    extra

--- hola.py
+++ hola.py
@@ -7,5 +7,25 @@ app = Flask(__name__)
 def url_principal():
 	return render_template("template.html", nombre="Lizeth", bienvenida="Hola, y bienvenido a mi blog.", about="Sobre mi...")
 
+@app.route("/librosJ")
+def url_librosJ():
+ 	tipos = [
+ 		"El psicoanalísta",
+ 		"Jaque al psicoanalísta"
+ 		"Un final perfecto"
+ 		"La historia del loco"
+ 	]
+ 	return render_template("librosJ.html", tipos=tipos, nombre="Titulos")
+
+ @app.route("/librosH")
+def url_librosH():
+ 	tipos = [
+ 		"El extranjero",
+ 		"El necronomicon"
+ 		"Los gatos de ulthar"
+ 		"El caso de Charles Dexter Ward"
+ 	]
+ 	return render_template("librosH.html", tipos=tipos, nombre="Titulos")
+
 if __name__=="__main__":
 	app.run(debug=True)
