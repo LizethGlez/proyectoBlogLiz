4ab6b867ba324ea7f62e5adf0bab90baea795623
LizethGlez <61913321+LizethGlez@users.noreply.github.com>
2020-05-20 00:17:41 -0500

    Actualización

--- hola.py
+++ hola.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def url_principal():
-	return render_template("template.html", nombre="Lizeth", bienvenida="Hola, y bienvenido a mi blog.", about="Sobre mi...")
+	return render_template("template.html")
 
 if __name__=="__main__":
 	app.run(debug=True)
